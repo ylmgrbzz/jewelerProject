9d093115b451e910a687452c70cb5f8188f6867d
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-12-18 23:05:31 +0300

    JewelerPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import RoutesApp from "./routes/Routes";
 
 function App() {
