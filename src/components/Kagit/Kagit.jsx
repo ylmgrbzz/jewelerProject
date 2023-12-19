ee911c65b823eedb5bb9acb7a43db3d15d6979dc
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-12-19 23:10:51 +0300

    Kagit ve takoz

--- src/components/Kagit/Kagit.jsx
+++ src/components/Kagit/Kagit.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./Kagit.css";
+import { useNavigate } from "react-router-dom";
 
 const Kagit = () => {
-  return <div>Kagit</div>;
+  const navigate = useNavigate();
+
+  const backToPage = () => {
+    navigate("/jeweler");
+  };
+  return (
+    <div class="container">
+      <div>
+        <a href="#" class="btn btn-primary back-button" onClick={backToPage()}>
+          Geri Dön
+        </a>
+      </div>
+      <h1 class="menu-title">KAĞIT EKRANI</h1>
+
+      <a href="/kagit/alis" class="btn btn-primary btn-lg menu-button">
+        Kağıt Alış
+      </a>
+      <a href="/kagit/satis" class="btn btn-success btn-lg menu-button">
+        Kağıt Satış
+      </a>
+      <a href="/kagit/tasima" class="btn btn-secondary btn-lg menu-button">
+        Kağıt Taşıma
+      </a>
+    </div>
+  );
 };
 
 export default Kagit;
--- src/components/Takoz/Takoz.jsx
+++ src/components/Takoz/Takoz.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./Takoz.css";
+import { useNavigate } from "react-router-dom";
 
 const Takoz = () => {
-  return <div>Takoz</div>;
+  const navigate = useNavigate();
+
+  const backToPage = () => {
+    navigate("/jeweler");
+  };
+
+  return (
+    <div class="container">
+      <div>
+        <a href="#" class="btn btn-primary back-button" onClick={backToPage()}>
+          Geri Dön
+        </a>
+      </div>
+      <h1 class="menu-title">TAKOZ EKRANI</h1>
+
+      <a href="/takoz/alis" class="btn btn-primary btn-lg menu-button">
+        Takoz Alış
+      </a>
+      <a href="/takoz/satis" class="btn btn-success btn-lg menu-button">
+        Takoz Satış
+      </a>
+      <a href="/takoz/tasima" class="btn btn-secondary btn-lg menu-button">
+        Takoz Taşıma
+      </a>
+    </div>
+  );
 };
 
 export default Takoz;
