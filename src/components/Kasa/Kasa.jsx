2374d771b272fe99fbffc5924927fbccdc5075f0
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-01-03 20:48:37 +0300

    handleInputChange

--- src/components/Kasa/Kasa.jsx
+++ src/components/Kasa/Kasa.jsx
@@ -1,10 +1,7 @@
 import "./Kasa.css";
-
 import React, { useEffect, useState } from "react";
-
 import { Link } from "react-router-dom";
 import api from "../../services/api";
-import { useNavigate } from "react-router-dom";
 
 const Kasa = () => {
   const [kaydedenKisiFilter, setKaydedenKisiFilter] = useState("");
