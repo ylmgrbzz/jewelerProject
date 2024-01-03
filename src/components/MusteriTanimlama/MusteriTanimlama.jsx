72ff247044fe6f150f7689f3babca430d2baac5d
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-01-03 20:33:09 +0300

    MusteriTanimlama

--- src/components/MusteriTanimlama/MusteriTanimlama.jsx
+++ src/components/MusteriTanimlama/MusteriTanimlama.jsx
@@ -1,14 +1,9 @@
 import "./MusteriTanimlama.css";
-
-import React, { useEffect, useState } from "react";
-
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
-import { useNavigate } from "react-router-dom";
 
 const MusteriTanimlama = () => {
-  const navigate = useNavigate();
-
   const [formData, setFormData] = useState({
     unvan: "",
     ad_soyad: "",
